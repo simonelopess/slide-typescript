744d3d6614d3b8113958deb68f024ca02701a35f
Simone Lopes <sisilopes20@gmail.com>
2022-12-01 11:27:39 -0300

    estrutura inicial do slide

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import Slide from './Slide';
+
+
+const container = document.getElementById("slide");
+const elements = document.getElementById("slide-elements");
+const controls = document.getElementById("slide-controls");
+
+if (container && elements && controls && elements.children.length) {
+    new Slide(container, Array.from(elements.children), controls, 3000)
+}
