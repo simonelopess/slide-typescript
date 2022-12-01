54900489a0366e4c282ab1fe68f849eaf11959e8
Simone Lopes <sisilopes20@gmail.com>
2022-12-01 19:26:58 -0300

    adiciona funcao para mostrar e esconder slide

--- src/Slide.ts
+++ src/Slide.ts
@@ -3,15 +3,27 @@ export default class Slide {
     elements;
     controls;
     time;
+    index;
+    slide;
     constructor(container: Element, elements: Element[], controls: Element, time: number = 5000) {
         this.container = container;
         this.elements = elements;
         this.controls = controls;
         this.time = time;
+        this.index = 0;
+        this.slide = this.elements[this.index]
 
-        console.log(this.container)
-        console.log(this.elements)
-        console.log(this.controls)
-        console.log(this.time)
+        this.show(this.index)
+    }
+
+    hide(el: Element) {
+        el.classList.remove('active');
+    }
+
+    show(index: number) {
+        this.index = index;
+        this.slide = this.elements[this.index];
+        this.elements.forEach(el => this.hide(el));
+        this.slide.classList.add("active")
     }
 }
